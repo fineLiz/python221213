6af4cdcaf6fa55829594256e2ea38fa84e7e507a
fineLiz <finedrop@naver.com>
2022-12-14 11:42:31 +0900

    클래스연습

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,11 +1,13 @@
+#전역변수
 str = "Not Class Member"
+#클래스 정의
 class GString:
     def __init__(self):
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        print(self.str)
 
 g = GString()
 g.set("First Message")

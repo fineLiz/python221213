f80c538919343709c262a68a2f848c183cd5b79f
fineLiz <finedrop@naver.com>
2022-12-15 15:25:55 +0900

    웹크롤링

---
+++ web1.py
@@ -0,0 +1,25 @@
+# web1.py
+from bs4 import BeautifulSoup
+
+#페이지 로딩
+page = open("C:\\work\\test01.html","rt", encoding="utf-8").read()
+#검색이 용이한 객체
+soup = BeautifulSoup(page, "html.parser")
+#전체 페이지 보기
+#print( soup.prettify())
+
+#<p>태그 전부 검색
+#print( soup.find_all("p"))
+#첫번째 <p>태그
+#print( soup.find("p"))
+
+#검색의 조건: <p class='outer-text'>
+#파이썬의 키워드 class제공(이름충돌): class_
+#print( soup.find_all("p", class_="outer-text"))
+
+
+#컨텐츠만
+for item in soup.find_all("p"):
+    title = item.text.strip()
+    title = title.replace("\n","")
+    print(title)
